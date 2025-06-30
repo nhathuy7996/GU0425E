d68464dab86efcc45bd5703fcc45d4fc4e4be391
nhathuy7996 <nhat.huy.7996@gmail.com>
2025-06-30 19:43:07 +0700

    save game

--- Assets/GAME/SCRIPTS/DATA/DataManager.cs
+++ Assets/GAME/SCRIPTS/DATA/DataManager.cs
@@ -8,34 +8,34 @@ public class DataManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerData data = new PlayerData();
-        data.playerName = "Huynn";
-        data.level = 10;
-        data.items = new itemData[3];
+        // PlayerData data = new PlayerData();
+        // data.playerName = "Huynn";
+        // data.level = 10;
+        // data.items = new itemData[3];
 
-        data.items[0] = new itemData();
-        data.items[0].name = "mana";
-        data.items[0].quantity = 1;
+        // data.items[0] = new itemData();
+        // data.items[0].name = "mana";
+        // data.items[0].quantity = 1;
 
-        data.items[1] = new itemData();
-        data.items[1].name = "potion";
-        data.items[1].quantity = 10;
+        // data.items[1] = new itemData();
+        // data.items[1].name = "potion";
+        // data.items[1].quantity = 10;
 
-        data.items[2] = new itemData();
-        data.items[2].name = "gun";
-        data.items[2].quantity = 1;
+        // data.items[2] = new itemData();
+        // data.items[2].name = "gun";
+        // data.items[2].quantity = 1;
 
-        string jsonConverted = JsonUtility.ToJson(data);
+        // string jsonConverted = JsonUtility.ToJson(data);
 
-        PlayerPrefs.SetString(typeof(PlayerData).ToString(), jsonConverted);
+        // PlayerPrefs.SetString(typeof(PlayerData).ToString(), jsonConverted);
 
-        string s = PlayerPrefs.GetString(typeof(PlayerData).ToString());
+        // string s = PlayerPrefs.GetString(typeof(PlayerData).ToString());
 
-        var dataSaved = JsonUtility.FromJson<PlayerData>(s);
+        // var dataSaved = JsonUtility.FromJson<PlayerData>(s);
 
 
 
-        Debug.Log(dataSaved);
+        // Debug.Log(dataSaved);
 
 
     }
@@ -43,15 +43,7 @@ public class DataManager : MonoBehaviour
 
 }
 
-[System.Serializable]
-public class PlayerData
-{
-    public string playerName;
-    public int level;
 
-    public itemData[] items;
-
-}
 
 [System.Serializable]
 public class itemData
--- Assets/GAME/SCRIPTS/GameManager.cs
+++ Assets/GAME/SCRIPTS/GameManager.cs
@@ -1,26 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Security;
 
 public class GameManager : Singleton<GameManager>
 {
-
-    [SerializeField]
-    float _level = 0; 
-
-    [SerializeField]
-    int _score = 0;
-
-    public int Score => _score;
  
+
+    [SerializeField]
+    PlayerData playerData;
+
+    public int Score => playerData.score;
+
+    void Start()
+    {
+        // this._level = PlayerPrefs.GetInt("Level");
+        // this._score = PlayerPrefs.GetInt("Score");
+
+        string json = PlayerPrefs.GetString(typeof(PlayerData).ToString(), "{}");
+        this.playerData = JsonUtility.FromJson<PlayerData>(json); 
+    }
+
 
     public void AddScore(int scr = 1)
     {
-        this._score += scr;
+        this.playerData.score += scr;
+        if (this.playerData.score >= this.playerData.maxScoreLevelUp)
+        {
+            this.playerData.level++;
+            this.playerData.score = 0;
+        }
+
+        PlayerPrefs.SetString(typeof(PlayerData).ToString(), JsonUtility.ToJson(this.playerData));
+
+        // PlayerPrefs.SetInt("Score", this._score);
+        // PlayerPrefs.SetInt("Level", this._level);
     }
 
     public void ResetScore()
     {
-        this._score = 0;
+        this.playerData.score = 0;
     }
+}
+
+
+[System.Serializable]
+public class PlayerData
+{
+    public int level;
+    public int score;
+
+    public int maxScoreLevelUp = 3;
 }
