754816698fb986ea9a4d47aa4cdbb65c4b0f3092
nhathuy7996 <nhat.huy.7996@gmail.com>
2025-07-25 19:42:06 +0700

    add parallax

--- Assets/GAME/SCRIPTS/LoopingBG.cs
+++ Assets/GAME/SCRIPTS/LoopingBG.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class LoopingBG : MonoBehaviour
 {
-    SpriteRenderer _spriteRenderer;
-    [SerializeField] float _widthImage;
+    SpriteRenderer[] _spriteRenderers;
+    [SerializeField] float[] _widthImage;
+    [SerializeField] float _speed;
+    [SerializeField] Rigidbody2D playerRigi;
    
    Camera _camera;
     // The speed at which the background loops
@@ -15,20 +16,43 @@ public class LoopingBG : MonoBehaviour
     void Start()
     {
         this._camera = Camera.main;
-        this._spriteRenderer = GetComponent<SpriteRenderer>();
-        Texture image = this._spriteRenderer.sprite.texture;
-        this._widthImage = image.width / this._spriteRenderer.sprite.pixelsPerUnit;
+        this._spriteRenderers = this.GetComponentsInChildren<SpriteRenderer>();
+        _widthImage = new float[this._spriteRenderers.Length];
+
+        for (int i = 0; i < this._spriteRenderers.Length; i++)
+        {
+            SpriteRenderer spriteRenderer = this._spriteRenderers[i];
+            Texture image = spriteRenderer.sprite.texture;
+            _widthImage[i] = image.width / spriteRenderer.sprite.pixelsPerUnit;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Math.Abs(this._camera.transform.position.x - this.transform.position.x) > this._widthImage)
-        {
-            Vector3 pos = this.transform.position;
-            pos.x = this._camera.transform.position.x;
+        int way = this.playerRigi.velocity.x == 0 ? 0 : this.playerRigi.velocity.x > 0? -1 : 1;
+        // if (this.playerRigi.velocity.x == 0)
+        // {
+        //     way = 0;
+        // }
+        // else 
+        // {
+        //     way = this.playerRigi.velocity.x > 0 ? 1 : -1;
+        // }
 
-            this.transform.position = pos;
+        for (int i = 0; i < this._spriteRenderers.Length; i++)
+        {
+            SpriteRenderer spriteRenderer = this._spriteRenderers[i];
+            if (Math.Abs(this._camera.transform.position.x - spriteRenderer.transform.position.x) > this._widthImage[i])
+            {
+                Vector3 pos = spriteRenderer.transform.position;
+                pos.x = this._camera.transform.position.x;
+
+                spriteRenderer.transform.position = pos;
+            }
+            spriteRenderer.transform.Translate(new Vector3(way,0,0) * this._speed * i * Time.deltaTime);
         }
+
+        
     }
 }
